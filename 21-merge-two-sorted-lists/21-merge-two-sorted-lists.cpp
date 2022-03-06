01b40b836d01496731a2eb8629217630a7f77a49
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-03-07 00:46:34 +0530

    Time: 11 ms (55.60%), Space: 14.8 MB (82.97%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,12 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def mergeTwoLists(self,l1,l2):
-        if l1 and l2:
-            if l1.val > l2.val:
-                l1, l2 = l2, l1
-            l1.next = self.mergeTwoLists(l1.next, l2);            
-        return l1 or l2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
+        if(list1->val < list2->val)
+        {
+            list1->next = mergeTwoLists(list1->next,list2);
+        }
+        else
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
+        }
+        return list1->val < list2->val ? list1 : list2;
+    }
+};
