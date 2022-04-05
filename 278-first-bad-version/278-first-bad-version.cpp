ebe16f612ca57c14d8d8031b73a06f4334f5260e
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-04-05 19:03:30 +0530

    Time: 3 ms (28.94%), Space: 5.8 MB (89.66%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,19 +4,13 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left(1), right(n);
-        while(left < right)
+        int start(0), end(n);
+        while(start < end)
         {
-            int mid = left + (right - left) / 2;
-            if (isBadVersion(mid))
-            {
-                right = mid;
-            }
-            else
-            {
-                left = mid + 1;
-            }
+            int mid = start + (end - start) / 2;
+            if(isBadVersion(mid)) end = mid;
+            else start = mid + 1;
         }
-        return left;
+        return end;
     }
 };
