217f3d7c03dd08ca0ea0794a4efa7cd41b1e6608
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-04-27 20:56:08 +0530

    Time: 730 ms (17.95%), Space: 17.8 MB (71.84%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,39 +1,24 @@
-class MinStack {
-public:
-    stack<pair<int,int>> st;
-    int x;
-    MinStack() {
-        
-    }
-    
-    void push(int val) {
-        x = val;
-        if(st.size())
-        {
-            x = st.top().second;
-            if(x > val) x = val;
-        }
-        st.push({val,x});
-    }
-    
-    void pop() {
-        st.pop();
-    }
-    
-    int top() {
-        return st.top().first;
-    }
-    
-    int getMin() {
-        return st.top().second;
-    }
-};
+class MinStack:
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+    def __init__(self):
+        self.st = []
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+
+    def pop(self) -> None:
+        self.st.pop()
+
+    def top(self) -> int:
+        return self.st[-1]
+
+    def getMin(self) -> int:
+        return min(self.st)
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
