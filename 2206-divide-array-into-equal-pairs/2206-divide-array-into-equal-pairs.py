8dc9fc7216c399ad96e02eda7a6f5725c5ee23e6
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-05-11 08:35:09 +0530

    Time: 85 ms (95.93%), Space: 14.2 MB (21.66%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,12 +1,4 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        umap = {}
-        for i in nums:
-            if i not in umap:
-                umap[i] = 1
-            else:
-                umap[i] += 1
-        for k,v in umap.items():
-            if v % 2:
-                return False
-        return True
+        return all( v % 2 == 0 for v in Counter(nums).values())
+    
