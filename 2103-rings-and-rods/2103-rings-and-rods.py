c175a1347bb4ec108353489fcb52958f9deb9f39
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-05-11 16:39:49 +0530

    Time: 41 ms (53.18%), Space: 13.8 MB (97.77%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,19 +1,6 @@
-class Solution {
-public:
-    int countPoints(string rings) {
-        int sz(rings.size());
-        vector<set<char>> arr(10);
-        for(int i = 0; i < sz - 1 ; i+=2)
-        {
-            char ch = rings[i];
-            int n = rings[i+1] - '0';
-            arr[n].insert(ch);
-        }
-        int cnt(0);
-        for(int i = 0; i < 10; i++)
-        {
-            if(arr[i].size() == 3 ) cnt++;
-        }
-        return cnt;
-    }
-};
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        d = defaultdict(set)
+        for i in range(0,len(rings) - 1, 2):
+            d[rings[i+1]].add(rings[i])
+        return sum(len(v) == 3 for v in d.values())
