68015bdcf9d40896fee640bf0a60ce2a1aa64a32
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-05-21 22:47:37 +0530

    Time: 46 ms (50.60%), Space: 13.8 MB (99.30%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,8 +1,4 @@
-class Solution {
-public:
-    int maxNumberOfBalloons(string text) {
-        unordered_map<char, int> mp(26);
-        for(char& ch : text) mp[ch]++;
-        return min({mp['b'],mp['a'],mp['l'] / 2 ,mp['o'] / 2, mp['n']});
-    }
-};
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        mp = Counter(text)
+        return min(mp["b"], mp["a"], mp["l"] // 2, mp["o"] // 2 , mp["n"])
