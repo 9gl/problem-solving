210a740e346785d8e4472d502b8719e650c4a455
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-05-21 23:24:08 +0530

    Time: 91 ms (20.15%), Space: 14.5 MB (67.40%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,21 +1,12 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        int mp[26] = {};
-        int n = s.size();
-        if(s.size() != t.size()) return false;
-        for(int i = 0; i < s.size() ;i++)
-        {
-            mp[s[i] - 'a']++;
-            mp[t[i] - 'a']--;
-        }
-        for(char& ch : s)
-        {
-            if(mp[ch - 'a'] > 0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-};
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mp = [0] * 26
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            mp[ord(s[i]) - ord('a')] += 1
+            mp[ord(t[i]) - ord('a')] -= 1
+        for i in s:
+            if(mp[ord(i) - ord('a')]) > 0:
+                return False
+        return True
