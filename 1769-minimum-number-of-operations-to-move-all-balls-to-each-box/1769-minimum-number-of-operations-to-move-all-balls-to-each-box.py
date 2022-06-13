54f00010398bdf628181a119c73eb9e9807a782a
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-06-13 13:04:45 +0530

    Time: 122 ms (66.33%), Space: 14.2 MB (85.60%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    vector<int> minOperations(string boxes) {
-        int sz = boxes.size();
-        vector<int> v(sz);
-        for(int i = 0; i < sz; i++)
-        {
-            int move(0);
-            for(int j = 0; j < sz; j++)
-            {
-                if(boxes[j] == '1')
-                {
-                    move+=abs(i -j);
-                }
-            }
-            v[i] = move;
-        }
-        return v;
-    }
-};
+class Solution:
+    def minOperations(self, b: str) -> List[int]:
+        sz = len(b)
+        pre, suff = [0]*sz,[0]*sz
+        pre[0] = 0
+        suff[sz-1] = 0
+        cnt = 0
+        for i in range(1,sz):
+            if b[i-1] == '1':
+                cnt += 1
+            pre[i] = pre[i-1] + cnt
+        cnt = 0
+        for i in range(sz-2,-1,-1):
+            if b[i+1] == '1':
+                cnt+=1
+            suff[i] = suff[i+1] + cnt
+        l = [0] * sz
+        for i in range(sz):
+            l[i] = pre[i]+suff[i]
+        return l
+            
+        
