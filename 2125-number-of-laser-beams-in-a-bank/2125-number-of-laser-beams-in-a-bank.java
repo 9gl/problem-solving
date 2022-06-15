a6ca0c9475a320c4f286d8007b072cfd47b6cb81
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-06-15 14:11:11 +0530

    Time: 22 ms (54.57%), Space: 54.7 MB (36.65%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,17 +1,22 @@
-class Solution:
-    def numberOfBeams(self, b: List[str]) -> int:
-        l = []
-        for s in b:
-            cnt = 0
-            for ch in s:
-                if ch == '1':
-                    cnt += 1
-            if cnt != 0:
-                l.append(cnt)
-        r = 0
-        j = 1
-        for i in range(len(l)-1):
-            r += l[i]*l[j]
-            j+=1
-        return r
-            
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int res = 0 , pre = 0;
+        for(String s : bank)
+        {
+            int cnt = 0;
+            for(int i = 0; i < s.length(); i++)
+            {
+                if(s.charAt(i) == '1')
+                {
+                    cnt++;
+                }
+            }
+            if(cnt > 0)
+            {
+                res += cnt * pre;
+                pre = cnt;
+            }
+        }
+        return res;
+    }
+}
